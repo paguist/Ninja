b5bdfacc80f9452623584bb197c98137a3f783a7
paguist <paguistt@gmail.com>
2024-06-16 10:00:44 -0400

    [TAREA] sensei

--- ninja.js
+++ ninja.js
@@ -21,10 +21,31 @@ class Ninja {
     }
   }
   
-  // Prueba de uso
-  const ninja1 = new Ninja("Hyabusa");
+  /* Prueba de uso
+  const ninja1 = new Ninja("Unagi");
   ninja1.sayName();
   ninja1.showStats();
   ninja1.drinkSake();
   ninja1.showStats();
+   */
+
+  // Tarea Super Ninja
+  class Sensei extends Ninja {
+    constructor(nombre) {
+      super(nombre);
+      this.salud = 200;
+      this.velocidad = 10;
+      this.fuerza = 10;
+      this.sabiduría = 10;
+    }
   
+    speakWisdom() {
+      this.drinkSake();
+      console.log("Programar es 10% escribir código y 90% entender por qué no funciona.");
+    }
+  }
+  
+  // Prueba
+  const superSensei = new Sensei("Master Unagi");
+  superSensei.speakWisdom();
+  superSensei.showStats();
